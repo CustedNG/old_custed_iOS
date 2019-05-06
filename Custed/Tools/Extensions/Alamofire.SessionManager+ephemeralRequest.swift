7bcc4a7cd2b16bfb62c16346454aff3b3bd5d3ed
NeZha <bsheep0734@gmail.com>
2019-05-06 20:38:15 +0800

    no message

--- Custed/Tools/Extensions/Alamofire.SessionManager+ephemeralRequest.swift
+++ Custed/Tools/Extensions/Alamofire.SessionManager+ephemeralRequest.swift
@@ -14,6 +14,8 @@ extension SessionManager {
         configuration.httpAdditionalHeaders = SessionManager.defaultHTTPHeaders
         configuration.timeoutIntervalForRequest = TimeInterval.init(5.0)
         configuration.timeoutIntervalForResource = TimeInterval.init(10.0)
+        configuration.httpCookieAcceptPolicy = HTTPCookie.AcceptPolicy.never
+        configuration.httpShouldSetCookies = false // Don't set cookies for request through this property
         return SessionManager(configuration: configuration)
     }()
     public static let timeOut : SessionManager = {
@@ -21,6 +23,8 @@ extension SessionManager {
         config.httpAdditionalHeaders = SessionManager.defaultHTTPHeaders
         config.timeoutIntervalForRequest = TimeInterval.init(5.0)
         config.timeoutIntervalForResource = TimeInterval.init(10.0)
+        config.httpCookieAcceptPolicy = HTTPCookie.AcceptPolicy.onlyFromMainDocumentDomain // default setting
+        config.httpShouldSetCookies = true
         return SessionManager(configuration: config)
     }()
 }
